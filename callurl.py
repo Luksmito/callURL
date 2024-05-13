0e05b0e12fea558c7dd07428883a4f0c158809e3
Luksmito <lucasmaxgames@gmail.com>
2024-05-13 19:16:10 -0300

    first commit

--- callurl.py
+++ callurl.py
@@ -1,6 +1,24 @@
+from flask import Flask, request
 import requests
 
-url = "https://:l-VrwV_BQC-XhdkhF6VYQA@tandem.autodesk.com/api/v1/timeseries/models/urn:adsk.dtm:65yT5kH4R42K6Op8xAP_uw/streams/AQAAAJsv-v3IAUEWpnJjjtkN0vMAAAAA"
-data = [{"umidade": 37}]
-response = requests.post(url, json=data)
+app = Flask(__name__)
 
+# URL para onde será feita a requisição
+url = "https://:l-VrwV_BQC-XhdkhF6VYQA@tandem.autodesk.com/api/v1/timeseries/models/urn:adsk.dtm:65yT5kH4R42K6Op8xAP_uw/streams/AQAAAJsv-v3IAUEWpnJjjtkN0vMAAAAA"
+
+@app.route('/receber_numero', methods=['POST'])
+def receber_numero():
+    # Obtém o número do corpo da requisição
+    numero = request.form.get('numero')
+    
+    # Constrói o corpo da nova requisição
+    data = [{"umidade": numero}]
+    
+    # Faz a requisição para a URL fornecida
+    response = requests.post(url, json=data)
+    
+    # Retorna a resposta da requisição feita para a URL
+    return response.text, response.status_code
+
+if __name__ == '__main__':
+    app.run(debug=True)
