270c6bb76ee6668297b363da5b3683ecaa42fc28
Luksmito <lucasmaxgames@gmail.com>
2024-05-13 19:33:29 -0300

    requirementsAdd

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 # URL para onde será feita a requisição
 url = "https://:l-VrwV_BQC-XhdkhF6VYQA@tandem.autodesk.com/api/v1/timeseries/models/urn:adsk.dtm:65yT5kH4R42K6Op8xAP_uw/streams/AQAAAJsv-v3IAUEWpnJjjtkN0vMAAAAA"
 
+@app.route("/")
+
+def hello():
+    return "hello"
 @app.route('/receber_numero', methods=['POST'])
 def receber_numero():
     # Obtém o número do corpo da requisição
